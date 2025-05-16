2ca726de636d693e5bb1da80d7819497511208c8
gustavo <gjuantorena@gmail.com>
2025-05-16 14:56:59 -0300

    radius changed

--- src/config.py
+++ src/config.py
@@ -27,7 +27,7 @@ LOGGING_CONFIG = {
 FIGURES_DIR = os.path.join(BASE_DIR, "figures")
 
 # Screen parameters
-RADIUS_HEIGHT = 0.05
+RADIUS_HEIGHT = 0.025
 
 # TMT parameters
 CORRECT_THRESHOLD = None  # 8
