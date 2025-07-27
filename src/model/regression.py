4c7fa28a0ee9d98b5aab35f88d63e0ece4f4bea4
gianluca-capelo <gianluca.capelo@despegar.com>
2025-07-27 20:07:52 -0300

    refactor

--- src/model/regression.py
+++ src/model/regression.py
@@ -7,12 +7,13 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_selection import SelectKBest, f_classif, f_regression
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.model_selection import GridSearchCV, StratifiedKFold, KFold
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, KFold, LeaveOneOut
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 
-from src.model.classification import calculate_feature_importance_for_fold
+from src.model.classification import calculate_feature_importance_for_fold, calculate_metrics_leave_one_out, perform, \
+    save_results, retrieve_dataset
 
 
 def get_parameter_grid():
@@ -54,8 +55,25 @@ def get_models(random_state: int):
     ]
 
 
+def perform(perform_pca: bool, dataset_name: str, global_seed: int,
+            inner_cv_seed: int, feature_selection: bool, tune_hyperparameters: bool, target_col):
+    X, y, feature_names = retrieve_dataset(dataset_name, target_col)
+
+    param_grids = get_parameter_grid()
+
+    models = get_models(global_seed)
+
+    outer_cv = LeaveOneOut()
+
+    performance_metrics_df = perform_cross_validation(param_grids, models, outer_cv, X, y, perform_pca,
+                                                      feature_selection, tune_hyperparameters,
+                                                      inner_cv_seed, feature_names)
+
+    return performance_metrics_df
+
+
 def perform_cross_validation(param_grids, models, outer_cv, X, y, perform_pca: bool, feature_selection: bool,
-                             tune_hyperparameters: bool, inner_cv_seed: int, feature_names):
+                             tune_hyperparameters: bool, inner_cv_seed: int, feature_names, is_classification):
     all_fold_metrics = []
 
     for model in models:
@@ -69,20 +87,17 @@ def perform_cross_validation(param_grids, models, outer_cv, X, y, perform_pca: b
                                                           feature_selection,
                                                           tune_hyperparameters,
                                                           inner_cv_seed,
-                                                          feature_names)
+                                                          feature_names, is_classification)
 
         all_fold_metrics.extend(fold_metrics)
 
     return pd.DataFrame(all_fold_metrics)
 
 
-is_classification = False
-
-
 def perform_cross_validation_for_model(param_grid, model, outer_cv, X, y, perform_pca: bool, feature_selection: bool,
                                        tune_hyperparameters: bool, inner_cv_seed: int,
-                                       feature_names):
-    if (is_classification):
+                                       feature_names, is_classification):
+    if is_classification:
         select_score_func = f_classif
         pipeline_name = 'classifier'
     else:
@@ -123,8 +138,11 @@ def perform_cross_validation_for_model(param_grid, model, outer_cv, X, y, perfor
         ])
 
         if tune_hyperparameters and param_grid:
-            inner_cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=inner_cv_seed) if is_classification \
-                else (KFold(n_splits=3, shuffle=True, random_state=inner_cv_seed))
+            inner_cv = (
+                StratifiedKFold(n_splits=3, shuffle=True, random_state=inner_cv_seed)
+                if is_classification
+                else KFold(n_splits=3, shuffle=True, random_state=inner_cv_seed)
+            )
             scoring = 'roc_auc' if is_classification else 'r2'
             grid = GridSearchCV(pipeline, param_grid=param_grid, cv=inner_cv, scoring=scoring, n_jobs=-1, verbose=0)
             grid.fit(X_train, y_train)
@@ -155,3 +173,36 @@ def perform_cross_validation_for_model(param_grid, model, outer_cv, X, y, perfor
         })
 
     return fold_metrics
+
+
+def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    )
+
+    global_seed = 42
+    inner_cv_seed = 50  # Fixed for reproducibility in inner CV
+    tune_hyperparameters = False
+    feature_selection = True
+    dataset_name = 'demographic+digital'
+    perform_pca = False
+    target_col = 'mmse' #TODO GIAN: AGREGAR
+    performance_metrics_df = perform(
+        perform_pca=perform_pca,
+        dataset_name=dataset_name,
+        global_seed=global_seed,
+        inner_cv_seed=inner_cv_seed,
+        feature_selection=feature_selection,
+        tune_hyperparameters=tune_hyperparameters,
+        target_col=target_col
+    )
+
+    leave_one_out_metrics_df = calculate_metrics_leave_one_out(performance_metrics_df)
+
+    save_results(leave_one_out_metrics_df, dataset_name, feature_selection, perform_pca, performance_metrics_df,
+                 tune_hyperparameters)
+
+
+if __name__ == "__main__":
+    main()
