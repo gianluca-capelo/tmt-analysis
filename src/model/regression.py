0a16b59ad6d6746df8c91564e8bdf1cbbc29a58c
gianluca-capelo <gianluca.capelo@despegar.com>
2025-07-27 19:52:55 -0300

    refactor

--- src/model/regression.py
+++ src/model/regression.py
@@ -4,10 +4,10 @@ import pandas as pd
 import xgboost as xgb
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import SelectKBest
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
@@ -74,9 +74,20 @@ def perform_cross_validation(param_grids, models, outer_cv, X, y, perform_pca: b
     return pd.DataFrame(all_fold_metrics)
 
 
+is_classification = False
 def perform_cross_validation_for_model(param_grid, model, outer_cv, X, y, perform_pca: bool, feature_selection: bool,
                                        tune_hyperparameters: bool, inner_cv_seed: int,
                                        feature_names):
+
+    if (is_classification):
+        select_score_func = f_classif
+        pipeline_name = 'classifier'
+        inner_cv = KFold(n_splits=3, shuffle=True, random_state=inner_cv_seed)
+    else:
+        select_score_func = f_regression
+        pipeline_name = 'regressor'
+        inner_cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=inner_cv_seed)
+
     max_pca_components = 4
     max_selected_features = 20
     model_name = model.__class__.__name__
@@ -94,37 +105,27 @@ def perform_cross_validation_for_model(param_grid, model, outer_cv, X, y, perfor
         # Steps
         pca_step = ('pca', PCA(n_components=min(max_pca_components, X_train.shape[1]))) if perform_pca else ('noop',
                                                                                                              'passthrough')
-        select_step = (
-            ('select', SelectKBest(score_func=f_classif, k=min(max_selected_features, X_train.shape[1])))
+
+        select_step = ('select', SelectKBest(score_func=f_regression, k=min(max_selected_features, X_train.shape[1]))) \
             if feature_selection else ('noop', 'passthrough')
-        )
 
         pipeline = Pipeline([
             ('imputer', SimpleImputer(strategy='mean')),
             select_step,
             ('scaler', StandardScaler()),
             pca_step,
-            ('classifier', model)
+            ('regressor', model)
         ])
 
         if tune_hyperparameters and param_grid:
-            inner_cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=inner_cv_seed)
-            grid = GridSearchCV(pipeline, param_grid=param_grid, cv=inner_cv, scoring='roc_auc', n_jobs=-1, verbose=0)
+            inner_cv = KFold(n_splits=3, shuffle=True, random_state=inner_cv_seed)
+            grid = GridSearchCV(pipeline, param_grid=param_grid, cv=inner_cv, scoring='r2', n_jobs=-1, verbose=0)
             grid.fit(X_train, y_train)
             best_model = grid.best_estimator_
         else:
             pipeline.fit(X_train, y_train)
             best_model = pipeline
 
-        # Only compute importance if PCA is OFF
-        if not perform_pca:
-            importance_dict = calculate_feature_importance_for_fold(X_train, best_model, feature_names,
-                                                                    feature_selection, model_name)
-        else:
-            importance_dict = {}
-            logging.info(f"Skipping feature importance for {model_name} in fold {fold} due to PCA.")
-
-        y_pred_proba = best_model.predict_proba(X_test)[:, 1]
         y_pred = best_model.predict(X_test)
 
         fold_metrics.append({
@@ -132,8 +133,7 @@ def perform_cross_validation_for_model(param_grid, model, outer_cv, X, y, perfor
             'fold': fold,
             'y_test': y_test[0],
             'y_pred': y_pred[0],
-            'y_pred_proba': y_pred_proba[0],
-            'feature_importances': importance_dict,
+            'feature_importances': None,  # TODO GIAN: CALCULAR
             'feature_names': feature_names
         })
 
