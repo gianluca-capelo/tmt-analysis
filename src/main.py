a20ce18839713a172bebaa9b92428fb0862fa957
gianluca-capelo <gianluca.capelo@despegar.com>
2025-08-12 15:39:06 -0300

    refactor

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from src import config
 from src.data_processing.filter_data_with_last_date import filter_data_with_last_date
 from src.data_processing.mock_subjects import mock_subjects
@@ -18,7 +21,15 @@ filtered_by_date_dir = config.FILTERED_DATA_DIR
 
 filter_data_with_last_date(no_placeholders_dir, filtered_by_date_dir)
 
+# Delete the placeholders directory after filtering
+if os.path.exists(no_placeholders_dir):
+    shutil.rmtree(no_placeholders_dir)
+
 # Process the data to convert it to BIDS format and compute derivatives
 process_data(source_path=filtered_by_date_dir)
+
+# Delete the filtered_by_date_dir after processing
+if os.path.exists(filtered_by_date_dir):
+    shutil.rmtree(filtered_by_date_dir)
 
 run_analysis()
