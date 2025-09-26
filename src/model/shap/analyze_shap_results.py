11bf842cacb56e73bc668f8e492f10d1ebe2092f
gustavo <gjuantorena@gmail.com>
2025-09-26 18:16:02 -0300

    full dict of features

--- src/model/shap/analyze_shap_results.py
+++ src/model/shap/analyze_shap_results.py
@@ -138,30 +138,146 @@ def plot_shap_summary(df, is_classification, target_col, top_n=20, plot_freq=Fal
     bar_color = "steelblue"
 
 
-    # Warning: Complete feature labels for classification, need to be completed for regression
+
     feature_labels = {
         "is_valid_sum_A": "Valid sum (Part A)",
-        "non_cut_correct_targets_touches_PART_B": "Complete correct touches (Part B)",
         "is_valid_sum_B": "Valid sum (Part B)",
-        "max_duration_B_A_ratio": "Hesitation Max duration (B/A ratio)",
+
+        "area_difference_from_ideal_PART_A": "Area difference from ideal (Part A)",
+        "area_difference_from_ideal_PART_B": "Area difference from ideal (Part B)",
+        "area_difference_from_ideal_B_A_ratio": "Area difference from ideal (B/A ratio)",
+
+        "average_duration_PART_A": "Average duration (Part A)",
+        "average_duration_PART_B": "Average duration (Part B)",
+        "average_duration_B_A_ratio": "Average duration (B/A ratio)",
+
+        "distance_difference_from_ideal_PART_A": "Distance difference from ideal (Part A)",
+        "distance_difference_from_ideal_PART_B": "Distance difference from ideal (Part B)",
+        "distance_difference_from_ideal_B_A_ratio": "Distance difference from ideal (B/A ratio)",
+
+        "hesitation_avg_speed_PART_A": "Hesitation average speed (Part A)",
+        "hesitation_avg_speed_PART_B": "Hesitation average speed (Part B)",
+        "hesitation_avg_speed_B_A_ratio": "Hesitation average speed (B/A ratio)",
+
+        "hesitation_distance_PART_A": "Hesitation distance (Part A)",
+        "hesitation_distance_PART_B": "Hesitation distance (Part B)",
+        "hesitation_distance_B_A_ratio": "Hesitation distance (B/A ratio)",
+
+        "hesitation_time_PART_A": "Hesitation time (Part A)",
+        "hesitation_time_PART_B": "Hesitation time (Part B)",
+        "hesitation_time_B_A_ratio": "Hesitation time (B/A ratio)",
+
+        "inter_target_time_PART_A": "Inter-target time (Part A)",
+        "inter_target_time_PART_B": "Inter-target time (Part B)",
+        "inter_target_time_B_A_ratio": "Inter-target time (B/A ratio)",
+
+        "intra_target_time_PART_A": "Intra-target time (Part A)",
+        "intra_target_time_PART_B": "Intra-target time (Part B)",
+        "intra_target_time_B_A_ratio": "Intra-target time (B/A ratio)",
+
+        "max_duration_PART_A": "Hesitation max duration (Part A)",
+        "max_duration_PART_B": "Hesitation max duration (Part B)",
+        "max_duration_B_A_ratio": "Hesitation max duration (B/A ratio)",
+
+        "mean_abs_acceleration_PART_A": "Mean absolute acceleration (Part A)",
+        "mean_abs_acceleration_PART_B": "Mean absolute acceleration (Part B)",
+        "mean_abs_acceleration_B_A_ratio": "Mean absolute acceleration (B/A ratio)",
+
+        "mean_acceleration_PART_A": "Mean acceleration (Part A)",
+        "mean_acceleration_PART_B": "Mean acceleration (Part B)",
+        "mean_acceleration_B_A_ratio": "Mean acceleration (B/A ratio)",
+
+        "mean_negative_acceleration_PART_A": "Mean negative acceleration (Part A)",
+        "mean_negative_acceleration_PART_B": "Mean negative acceleration (Part B)",
+        "mean_negative_acceleration_B_A_ratio": "Mean negative acceleration (B/A ratio)",
+
+        "mean_speed_PART_A": "Mean speed (Part A)",
+        "mean_speed_PART_B": "Mean speed (Part B)",
+        "mean_speed_B_A_ratio": "Mean speed (B/A ratio)",
+
+        "non_cut_correct_targets_touches_PART_A": "Complete correct touches (Part A)",
+        "non_cut_correct_targets_touches_PART_B": "Complete correct touches (Part B)",
         "non_cut_correct_targets_touches_B_A_ratio": "Complete correct touches (B/A ratio)",
+
+        "non_cut_rt_PART_A": "Complete time to complete trial (Part A)",
+        "non_cut_rt_PART_B": "Complete time to complete trial (Part B)",
+        "non_cut_rt_B_A_ratio": "Complete time to complete trial (B/A ratio)",
+
+        "peak_abs_acceleration_PART_A": "Peak absolute acceleration (Part A)",
+        "peak_abs_acceleration_PART_B": "Peak absolute acceleration (Part B)",
+        "peak_abs_acceleration_B_A_ratio": "Peak absolute acceleration (B/A ratio)",
+
+        "peak_acceleration_PART_A": "Peak acceleration (Part A)",
+        "peak_acceleration_PART_B": "Peak acceleration (Part B)",
+        "peak_acceleration_B_A_ratio": "Peak acceleration (B/A ratio)",
+
+        "peak_negative_acceleration_PART_A": "Peak negative acceleration (Part A)",
+        "peak_negative_acceleration_PART_B": "Peak negative acceleration (Part B)",
+        "peak_negative_acceleration_B_A_ratio": "Peak negative acceleration (B/A ratio)",
+
+        "peak_speed_PART_A": "Peak speed (Part A)",
+        "peak_speed_PART_B": "Peak speed (Part B)",
+        "peak_speed_B_A_ratio": "Peak speed (B/A ratio)",
+
+        "rt_PART_A": "Time to complete trial (Part A)",
+        "rt_PART_B": "Time to complete trial (Part B)",
+        "rt_B_A_ratio": "Time to complete trial (B/A ratio)",
+
+        "search_avg_speed_PART_A": "Search average speed (Part A)",
+        "search_avg_speed_PART_B": "Search average speed (Part B)",
+        "search_avg_speed_B_A_ratio": "Search average speed (B/A ratio)",
+
+        "search_distance_PART_A": "Search distance (Part A)",
+        "search_distance_PART_B": "Search distance (Part B)",
+        "search_distance_B_A_ratio": "Search distance (B/A ratio)",
+
+        "search_time_PART_A": "Search time (Part A)",
+        "search_time_PART_B": "Search time (Part B)",
+        "search_time_B_A_ratio": "Search time (B/A ratio)",
+
+        "state_transitions_PART_A": "State transitions (Part A)",
+        "state_transitions_PART_B": "State transitions (Part B)",
+        "state_transitions_B_A_ratio": "State transitions (B/A ratio)",
+
+        "std_abs_acceleration_PART_A": "STD absolute acceleration (Part A)",
+        "std_abs_acceleration_PART_B": "STD absolute acceleration (Part B)",
+        "std_abs_acceleration_B_A_ratio": "STD absolute acceleration (B/A ratio)",
+
+        "std_acceleration_PART_A": "STD acceleration (Part A)",
+        "std_acceleration_PART_B": "STD acceleration (Part B)",
+        "std_acceleration_B_A_ratio": "STD acceleration (B/A ratio)",
+
+        "std_negative_acceleration_PART_A": "STD negative acceleration (Part A)",
+        "std_negative_acceleration_PART_B": "STD negative acceleration (Part B)",
+        "std_negative_acceleration_B_A_ratio": "STD negative acceleration (B/A ratio)",
+
+        "std_speed_PART_A": "STD speed (Part A)",
+        "std_speed_PART_B": "STD speed (Part B)",
+        "std_speed_B_A_ratio": "STD speed (B/A ratio)",
+
+        "total_distance_PART_A": "Total distance (Part A)",
+        "total_distance_PART_B": "Total distance (Part B)",
+        "total_distance_B_A_ratio": "Total distance (B/A ratio)",
+
+        "total_hesitations_PART_A": "Hesitations (Part A)",
+        "total_hesitations_PART_B": "Hesitations (Part B)",
+        "total_hesitations_B_A_ratio": "Hesitations (B/A ratio)",
+
+        "travel_avg_speed_PART_A": "Travel average speed (Part A)",
+        "travel_avg_speed_PART_B": "Travel average speed (Part B)",
+        "travel_avg_speed_B_A_ratio": "Travel average speed (B/A ratio)",
+
+        "travel_distance_PART_A": "Travel distance (Part A)",
+        "travel_distance_PART_B": "Travel distance (Part B)",
+        "travel_distance_B_A_ratio": "Travel distance (B/A ratio)",
+
         "travel_time_PART_A": "Travel time (Part A)",
         "travel_time_PART_B": "Travel time (Part B)",
-        "total_hesitations_PART_A": "Hesitations (Part A)",
-        "travel_distance_B_A_ratio": "Travel distance (B/A ratio)",
-        "state_transitions_PART_A": "State transitions (Part A)",
-        "average_duration_PART_A": "Average duration (Part A)",
-        "non_cut_rt_PART_B": "Complete reaction time (Part B)",
-        "age": "Age",
-        "search_time_PART_A": "Search time (Part A)",
-        "max_duration_PART_B": "Hesitation max duration (Part B)",
-        "state_transitions_B_A_ratio": "State transitions (B/A ratio)",
-        "average_duration_PART_B": "Average duration (Part B)",
-        "non_cut_correct_targets_touches_PART_A": "Complete correct touches (Part A)",
-        "travel_avg_speed_B_A_ratio": "Average speed (B/A ratio)",
-        "hesitation_time_PART_A": "Hesitation time (Part A)",
-        "travel_distance_PART_B": "Travel distance (Part B)",
+        "travel_time_B_A_ratio": "Travel time (B/A ratio)",
+
+        "age": "Age"
     }
+
     df_plot.index = df_plot.index.map(feature_labels)
     bars = ax1.barh(df_plot.index, df_plot["mean_abs_shap"], alpha=0.7)
     ax1.set_xlabel("Mean |SHAP| (across selected folds)")
@@ -241,5 +357,5 @@ def main(is_classification, timestamp):
 
 if __name__ == "__main__":
     is_classification = True
-    timestamp = "2025-09-14_0038" if is_classification else "2025-09-14_1008"
+    timestamp = "2025-09-14_0038 (clasificacion completo)" if is_classification else "2025-09-14_1008"
     main(is_classification, timestamp)
