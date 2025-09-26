ab28f6e0a03b00ff74d883279771a2f378c1cd4a
gianluca-capelo <gianluca.capelo@despegar.com>
2025-09-26 16:22:24 -0300

    clean repo

--- src/visualization/trial_plotting.py
+++ src/visualization/trial_plotting.py
@@ -4,20 +4,7 @@ from neurotask.tmt.metrics.speed_metrics import calculate_speeds_between_cursor_
 from neurotask.tmt.model.tmt_model import TMTTrial
 
 
-def plot_with_color(trial: TMTTrial, target_radius: float, color_by=None, show_start=False):
-    """
-    Plotea la trayectoria del cursor junto con los targets en un gráfico,
-    ajustando el tamaño del gráfico al tamaño del canvas y dibujando círculos
-    alrededor de los targets con su contenido. La trayectoria del cursor se
-    dibuja con puntos cuyos colores cambian en función del tiempo, la velocidad o la aceleración.
-    El primer clic en el target se destaca con un marcador especial.
-
-    Parameters:
-    - trial: TMTTrial, el trial con la trayectoria del cursor y los targets.
-    - target_radius: float, radio de los círculos que rodean los targets.
-    - color_by: str o None, 'time', 'speed' o 'acceleration'. Si es None no colorea el trazo.
-    """
-
+def plot_with_color(trial: TMTTrial, target_radius: float, color_by=None, show_start=False, cmap_name='Blues'):
     # Extraer la posición de los targets
     target_x = [target.position.x for target in trial.stimuli]
     target_y = [target.position.y for target in trial.stimuli]
@@ -29,24 +16,10 @@ def plot_with_color(trial: TMTTrial, target_radius: float, color_by=None, show_s
     cursor_y = [cursor_info.position.y for cursor_info in cursor_trail_from_first_click]
     cursor_times = [cursor_info.time for cursor_info in cursor_trail_from_first_click]
 
-    colors, norm = None, None
-
-    if color_by == 'time':
-        # Normalizar tiempos para que estén en el rango [0, 1]
-        norm = plt.Normalize(min(cursor_times), max(cursor_times))
-        colors = plt.cm.viridis(norm(cursor_times))  # Usar un mapa de colores para el tiempo
-    elif color_by == 'speed':
-        speeds = calculate_speeds_between_cursor_positions(trial)
-        speeds = [0] + speeds  # Para igualar el número de puntos con las velocidades calculadas
-        norm = plt.Normalize(min(speeds), max(speeds))
-        colors = plt.cm.viridis(norm(speeds))  # Usar un mapa de colores para la velocidad
-    elif color_by == 'acceleration':
-        accelerations = calculate_accelerations_between_cursor_positions(trial)
-        accelerations = [0, 0] + accelerations  # Igualar el número de puntos (2 primeros puntos sin aceleración)
-        norm = plt.Normalize(min(accelerations), max(accelerations))
-        colors = plt.cm.viridis(norm(accelerations))
-    elif color_by is not None:
-        raise ValueError("El parámetro color_by debe ser 'time', 'speed', 'acceleration' o None.")
+    if color_by is not None:
+        colors, norm = get_cursor_colors(cmap_name, color_by, cursor_times, trial)
+    else:
+        colors, norm = None, None
 
     # Crear el gráfico
     fig, ax = plt.subplots(figsize=(8, 8))
@@ -57,26 +30,13 @@ def plot_with_color(trial: TMTTrial, target_radius: float, color_by=None, show_s
             [cursor_x[i], cursor_x[i + 1]],
             [cursor_y[i], cursor_y[i + 1]],
             color=colors[i] if colors is not None else "black",
-            linewidth=2,
+            linewidth=1,
             zorder=4
         )
 
-    # Dibujar los targets
-    for x, y, content in zip(target_x, target_y, target_contents):
-        circle = plt.Circle((x, y), target_radius, color='red', alpha=0.3, zorder=5)
-        ax.add_patch(circle)
-        plt.text(x, y, content, color='black', fontsize=8, ha='center', va='center', zorder=6)
-
-    # Destacar el primer clic
-    if trial.start and show_start:
-        fc_x = trial.start.position.x
-        fc_y = trial.start.position.y
-        plt.scatter(fc_x, fc_y, color='cyan', edgecolor='black', s=100,
-                    label='Start', zorder=7, marker='o', alpha=0.3)
-
     # Solo agregar barra de color si hay colores
     if colors is not None and norm is not None:
-        sm = plt.cm.ScalarMappable(cmap='viridis', norm=norm)
+        sm = plt.cm.ScalarMappable(cmap=cmap_name, norm=norm)
         sm.set_array([])
         cbar_label = (
             'Time' if color_by == 'time'
@@ -85,15 +45,50 @@ def plot_with_color(trial: TMTTrial, target_radius: float, color_by=None, show_s
         )
         fig.colorbar(sm, ax=ax, label=cbar_label)
 
+    # Dibujar los targets
+    for x, y, content in zip(target_x, target_y, target_contents):
+        circle = plt.Circle((x, y), target_radius, color='steelblue', alpha=0.3, zorder=5)
+        ax.add_patch(circle)
+        plt.text(x, y, content, color='black', fontsize=8, ha='center', va='center', zorder=6)
+
     # Labels y formato
-    ax.set_xlabel('X Position')
-    ax.set_ylabel('Y Position')
-    ax.set_title('Cursor Trail with Targets')
+    ax.set_xlabel('X screen coordinate (pixels)')
+    ax.set_ylabel('Y screen coordinate (pixels)')
+    ax.set_title('Cursor tracking during a TMT trial')
 
     # Ensure equal aspect ratio
     ax.set_aspect('equal', adjustable='box')
 
+    # Destacar el primer clic
+    if trial.start and show_start:
+        plot_trial_start(ax, trial)
+
+    return fig
+
+
+def plot_trial_start(ax, trial):
+    fc_x = trial.start.position.x
+    fc_y = trial.start.position.y
+    plt.scatter(fc_x, fc_y, color='cyan', edgecolor='black', s=100,
+                label='Trial start', zorder=7, marker='o', alpha=0.3)
     # Add legend
     ax.legend()
 
-    return fig
+
+def get_cursor_colors(cmap_name, color_by, cursor_times, trial):
+    if color_by == 'time':
+        norm = plt.Normalize(min(cursor_times), max(cursor_times))
+        colors = plt.get_cmap(cmap_name)(norm(cursor_times))
+    elif color_by == 'speed':
+        speeds = calculate_speeds_between_cursor_positions(trial)
+        speeds = [0] + speeds  # Para igualar el número de puntos con las velocidades calculadas
+        norm = plt.Normalize(min(speeds), max(speeds))
+        colors = plt.cm.viridis(norm(speeds))  # Usar un mapa de colores para la velocidad
+    elif color_by == 'acceleration':
+        accelerations = calculate_accelerations_between_cursor_positions(trial)
+        accelerations = [0, 0] + accelerations  # Igualar el número de puntos (2 primeros puntos sin aceleración)
+        norm = plt.Normalize(min(accelerations), max(accelerations))
+        colors = plt.cm.viridis(norm(accelerations))
+    else:
+        raise ValueError("El parámetro color_by debe ser 'time', 'speed', 'acceleration'")
+    return colors, norm
