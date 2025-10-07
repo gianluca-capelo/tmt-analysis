5abe1bcb14c0fd07eaf352bf38712b14f7ddb6d6
gianluca-capelo <gianluca.capelo@despegar.com>
2025-10-07 13:04:01 -0300

    init repo

--- src/visualization/segmentation_plotting.py
+++ src/visualization/segmentation_plotting.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from neurotask.tmt.model.tmt_model import TMTTrial, CursorInfo
 from neurotask.tmt.segmentation.segmentation import classify_cursor_positions_with_hesitation
 
-from src.visualization.trial_plotting_labels import plot_with_labels, plot_with_labels_scatter
+from src.visualization.trial_plotting_labels import  plot_with_labels_scatter
 
 
 def plot_segmentation(trial: TMTTrial, target_radius: float, speed_threshold: float, cmap_name='tab10'):
@@ -14,8 +14,7 @@ def plot_segmentation(trial: TMTTrial, target_radius: float, speed_threshold: fl
     segmentation_labels = [label for (label, color) in segmentation]
 
     labels_title = 'Segmentation Labels'
-    plot_with_labels(trial, target_radius, segmentation_labels, labels_title=labels_title, cmap_name=cmap_name)
 
-    plot_with_labels_scatter(trial, target_radius, segmentation_labels, labels_title=labels_title, cmap_name=cmap_name)
+    fig = plot_with_labels_scatter(trial, target_radius, segmentation_labels, labels_title=labels_title, cmap_name=cmap_name)
 
-    plt.show()
+    return fig
